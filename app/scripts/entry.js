838490c14780d54e7776eff2c845803bac8aa495
Shannon Riester <shannonriester@gmail.com>
2016-09-06 19:48:49 -0500

    building routes and pages

---
+++ app/scripts/entry.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import router from './router';
+
+ReactDOM.render(router, document.getElementById('container'));
---
+++ app/scripts/router.js
@@ -0,0 +1,11 @@
+import { Router, hashHistory, IndexRoute, Route } from 'react-router';
+
+const router = (
+  <Router history={hashHistory}>
+    <Route component={MainPage}>
+
+    </Route>
+  </Router>
+);
+
+export default router;
