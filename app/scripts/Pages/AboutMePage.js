6b04494a354a5eb0d2c45cf94fd093f7f0ff9cff
Shannon Riester <shannonriester@gmail.com>
2016-09-06 23:13:24 -0500

    more styles to AboutMePage

--- app/scripts/Pages/AboutMePage.js
+++ app/scripts/Pages/AboutMePage.js
@@ -6,6 +6,7 @@ export default React.createClass({
   render() {
     return (
       <div className="about-me-page">
+        <Nav />
         <figure className="profile-figure" style={{backgroundImage: `url(/assets/ProfilePic.jpg)`}}></figure>
         <figcaption className="profile-figcaption">Hi, I'm Shannon!</figcaption>
         <section className="mission-section">
