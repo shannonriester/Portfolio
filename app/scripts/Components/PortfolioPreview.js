08a16104ea8abf828a1fdd3823ed8d3e422d43b7
Shannon Riester <shannonriester@gmail.com>
2016-09-10 12:23:03 -0500

    passing in new props for portfolio footer

--- app/scripts/Components/PortfolioPreview.js
+++ app/scripts/Components/PortfolioPreview.js
@@ -1,12 +1,103 @@
 import React from 'react';
 
 export default React.createClass({
+  componentWillReceiveProps(newProps) {
+    console.log(newProps);
+  },
   render() {
-
+    let footer;
+    if (this.props.key[0] === 0) {
+      footer = (
+          <footer className="preview-footer">
+            <section className="footer-content">
+              <p className="preview-about-app">A location-based, social-media app that allows you to search for dog-friendly places, find cute dogs, and meet other local dog-lovers!</p>
+              <ul className="icons-ul">
+                <li>
+                  <a className="portfolio-live-link" href="http://_waggle.surge.sh/">
+                    <i className="portfolio-icon fa fa-external-link" aria-hidden="true"></i>
+                    <label className="app-label"> Live site</label>
+                  </a>
+                </li>
+                <li>
+                  <a className="portfolio-live-link" href="https://github.com/shannonriester/Waggle">
+                    <i className="portfolio-icon fa fa-github-square" aria-hidden="true"></i>
+                    <label className="app-label"> GitHub</label>
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </footer>);
+    }
+    if (this.props.key[1] === 1) {
+      footer = (
+          <footer className="preview-footer">
+            <section className="footer-content">
+              <p className="preview-about-app">Beat the Bands, was built using Spotify's API. Users can vote up/down which artist they'd prefer see visit their city.</p>
+              <ul className="icons-ul">
+                <li>
+                  <a className="portfolio-live-link" href="https://shannonriester.github.io/Beat-The-Bands-day-38/">
+                    <i className="portfolio-icon fa fa-external-link" aria-hidden="true"></i>
+                    <label className="app-label"> Live site</label>
+                  </a>
+                </li>
+                <li>
+                  <a className="portfolio-live-link" href="https://github.com/shannonriester/Beat-The-Bands-day-38">
+                    <i className="portfolio-icon fa fa-github-square" aria-hidden="true"></i>
+                    <label className="app-label"> GitHub</label>
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </footer>);
+    }
+    if (this.props.key[2] === 2) {
+      footer = (
+          <footer className="preview-footer">
+            <section className="footer-content">
+              <p className="preview-about-app">Collaborating on a team can be hard, but my peers and I really pulled together, having so much fun pair-programming, and built this awesome game!</p>
+              <ul className="icons-ul">
+                <li>
+                  <a className="portfolio-live-link" href="https://marklyck.github.io/Pokemon/">
+                    <i className="portfolio-icon fa fa-external-link" aria-hidden="true"></i>
+                    <label className="app-label"> Live site</label>
+                  </a>
+                </li>
+                <li>
+                  <a className="portfolio-live-link" href="https://github.com/MarkLyck/Pokemon">
+                    <i className="portfolio-icon fa fa-github-square" aria-hidden="true"></i>
+                    <label className="app-label"> GitHub</label>
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </footer>);
+    }
+    if (this.props.key[3] === 3) {
+      footer = (
+          <footer className="preview-footer">
+            <section className="footer-content">
+              <p className="preview-about-app">My Jeopardy game renders random categories and questions from actual Jeopardy clues and questions. Play for yourself!</p>
+              <ul className="icons-ul">
+                <li>
+                  <a className="portfolio-live-link" href="http://jeopardy_sriester.surge.sh/">
+                    <i className="portfolio-icon fa fa-external-link" aria-hidden="true"></i>
+                    <label className="app-label"> Live site</label>
+                  </a>
+                </li>
+                <li>
+                  <a className="portfolio-live-link" href="https://github.com/shannonriester/Day-33-Jeopardy">
+                    <i className="portfolio-icon fa fa-github-square" aria-hidden="true"></i>
+                    <label className="app-label"> GitHub</label>
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </footer>);
+    }
 
     return (
       <li className="portfolio-preview portfolio-side-item waggle-preview" style={{backgroundImage:`url(${this.props.image})`}}>
-        
+      {footer}
       </li>
     );
   }
--- app/scripts/Pages/PortfolioPage.js
+++ app/scripts/Pages/PortfolioPage.js
@@ -22,147 +22,64 @@ export default React.createClass({
 
     }
   },
-  showFooterWaggle() {
-    this.setState({
-      footerWaggle: true,
-      waggleImgUrl: '/assets/Waggle/LandingPageBrunch.png',
-    });
+  showFooter() {
+    console.log('showing footer');
   },
-  showFooterBeatBands() {
-    this.setState({
-      footerBeatBands: true,
-      beatBandsImgUrl: '/assets/BeatTheBands/VoteModal.png',
-    });
-  },
-  showFooterPokemon() {
-    this.setState({
-      footerPokemon: true,
-      pokemonImgUrl: '/assets/PokemonGame/LandingPage.png',
-    });
-  },
-  showFooterJeopardy() {
-    this.setState({
-      footerJeopardy: true,
-      jeopardyImgUrl: '/assets/Jeopardy/QuestionModal.png'
-    });
-  },
-  hideFooterWaggle() {
-    this.setState({
-      footerWaggle: false,
-      waggleImgUrl: '/assets/Waggle/SearchResultsPage.png',
-    });
-  },
-  hideFooterBeatBands() {
-    this.setState({
-      footerBeatBands: false,
-      beatBandsImgUrl: '/assets/BeatTheBands/SearchPage.png',
-    });
-  },
-  hideFooterPokemon() {
-    this.setState({
-      footerPokemon: false,
-      pokemonImgUrl: '/assets/PokemonGame/LandingPage.png',
-    });
-  },
-  hideFooterJeopardy() {
-    this.setState({
-      footerJeopardy: false,
-      jeopardyImgUrl: '/assets/Jeopardy/GameBoard.png',
-    });
-  },
+  hideFooter() {},
+  // showFooterWaggle() {
+  //   this.setState({
+  //     footerWaggle: true,
+  //     waggleImgUrl: '/assets/Waggle/LandingPageBrunch.png',
+  //   });
+  // },
+  // showFooterBeatBands() {
+  //   this.setState({
+  //     footerBeatBands: true,
+  //     beatBandsImgUrl: '/assets/BeatTheBands/VoteModal.png',
+  //   });
+  // },
+  // showFooterPokemon() {
+  //   this.setState({
+  //     footerPokemon: true,
+  //     pokemonImgUrl: '/assets/PokemonGame/LandingPage.png',
+  //   });
+  // },
+  // showFooterJeopardy() {
+  //   this.setState({
+  //     footerJeopardy: true,
+  //     jeopardyImgUrl: '/assets/Jeopardy/QuestionModal.png'
+  //   });
+  // },
+  // hideFooterWaggle() {
+  //   this.setState({
+  //     footerWaggle: false,
+  //     waggleImgUrl: '/assets/Waggle/SearchResultsPage.png',
+  //   });
+  // },
+  // hideFooterBeatBands() {
+  //   this.setState({
+  //     footerBeatBands: false,
+  //     beatBandsImgUrl: '/assets/BeatTheBands/SearchPage.png',
+  //   });
+  // },
+  // hideFooterPokemon() {
+  //   this.setState({
+  //     footerPokemon: false,
+  //     pokemonImgUrl: '/assets/PokemonGame/LandingPage.png',
+  //   });
+  // },
+  // hideFooterJeopardy() {
+  //   this.setState({
+  //     footerJeopardy: false,
+  //     jeopardyImgUrl: '/assets/Jeopardy/GameBoard.png',
+  //   });
+  // },
   render() {
     let footerWaggle;
     let footerBeatBands;
     let footerPokemon;
     let footerJeopardy;
-    if (this.state.footerWaggle) {
-      footerWaggle = (
-          <footer className="preview-footer">
-            <section className="footer-content">
-              <p className="preview-about-app">A location-based, social-media app that allows you to search for dog-friendly places, find cute dogs, and meet other local dog-lovers!</p>
-              <ul className="icons-ul">
-                <li>
-                  <a className="portfolio-live-link" href="http://_waggle.surge.sh/">
-                    <i className="portfolio-icon fa fa-external-link" aria-hidden="true"></i>
-                    <label className="app-label"> Live site</label>
-                  </a>
-                </li>
-                <li>
-                  <a className="portfolio-live-link" href="https://github.com/shannonriester/Waggle">
-                    <i className="portfolio-icon fa fa-github-square" aria-hidden="true"></i>
-                    <label className="app-label"> GitHub</label>
-                  </a>
-                </li>
-              </ul>
-            </section>
-          </footer>);
-    }
-    if (this.state.footerBeatBands) {
-      footerBeatBands = (
-          <footer className="preview-footer">
-            <section className="footer-content">
-              <p className="preview-about-app">Beat the Bands, was built using Spotify's API. Users can vote up/down which artist they'd prefer see visit their city.</p>
-              <ul className="icons-ul">
-                <li>
-                  <a className="portfolio-live-link" href="https://shannonriester.github.io/Beat-The-Bands-day-38/">
-                    <i className="portfolio-icon fa fa-external-link" aria-hidden="true"></i>
-                    <label className="app-label"> Live site</label>
-                  </a>
-                </li>
-                <li>
-                  <a className="portfolio-live-link" href="https://github.com/shannonriester/Beat-The-Bands-day-38">
-                    <i className="portfolio-icon fa fa-github-square" aria-hidden="true"></i>
-                    <label className="app-label"> GitHub</label>
-                  </a>
-                </li>
-              </ul>
-            </section>
-          </footer>);
-    }
-    if (this.state.footerPokemon) {
-      footerPokemon = (
-          <footer className="preview-footer">
-            <section className="footer-content">
-              <p className="preview-about-app">Collaborating on a team can be hard, but my peers and I really pulled together, having so much fun pair-programming, and built this awesome game!</p>
-              <ul className="icons-ul">
-                <li>
-                  <a className="portfolio-live-link" href="https://marklyck.github.io/Pokemon/">
-                    <i className="portfolio-icon fa fa-external-link" aria-hidden="true"></i>
-                    <label className="app-label"> Live site</label>
-                  </a>
-                </li>
-                <li>
-                  <a className="portfolio-live-link" href="https://github.com/MarkLyck/Pokemon">
-                    <i className="portfolio-icon fa fa-github-square" aria-hidden="true"></i>
-                    <label className="app-label"> GitHub</label>
-                  </a>
-                </li>
-              </ul>
-            </section>
-          </footer>);
-    }
-    if (this.state.footerJeopardy) {
-      footerJeopardy = (
-          <footer className="preview-footer">
-            <section className="footer-content">
-              <p className="preview-about-app">My Jeopardy game renders random categories and questions from actual Jeopardy clues and questions. Play for yourself!</p>
-              <ul className="icons-ul">
-                <li>
-                  <a className="portfolio-live-link" href="http://jeopardy_sriester.surge.sh/">
-                    <i className="portfolio-icon fa fa-external-link" aria-hidden="true"></i>
-                    <label className="app-label"> Live site</label>
-                  </a>
-                </li>
-                <li>
-                  <a className="portfolio-live-link" href="https://github.com/shannonriester/Day-33-Jeopardy">
-                    <i className="portfolio-icon fa fa-github-square" aria-hidden="true"></i>
-                    <label className="app-label"> GitHub</label>
-                  </a>
-                </li>
-              </ul>
-            </section>
-          </footer>);
-    }
+
 
     let portfolioHover;
 
@@ -171,8 +88,8 @@ export default React.createClass({
       return (
         <PortfolioPreview
         key={i}
-        onMouseEnter={this.showFooter}
-        onMouseLeave={this.hideFooter}
+        showFooter={this.showFooter}
+        hideFooter={this.hideFooter}
         image={image}
         style={{backgroundImage: `url({image})`}}
         />);
@@ -209,7 +126,6 @@ export default React.createClass({
         <Element name="PortfolioPage" />
         <ul className="portfolio-sides">
           {portfolioImgs}
-
         </ul>
       </div>
     );
