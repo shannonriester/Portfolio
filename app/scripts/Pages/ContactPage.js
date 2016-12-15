1b6b03fd4a7ba3eebd9edc19ca42055c5eccf8cc
Shannon Riester <shannonriester@gmail.com>
2016-12-15 08:49:18 -0600

    add shake to failed email submit

--- app/scripts/Pages/ContactPage.js
+++ app/scripts/Pages/ContactPage.js
@@ -8,33 +8,47 @@ export default React.createClass({
   getInitialState() {
     return {
       sentEmail: false,
+      shake: false,
     }
+  },
+  validateEmail(email) {
+    let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (regex.exec(email)) return true;
+
+    return false;
   },
   contactMe(e) {
     e.preventDefault();
-    $.ajax({
-      url: `https://baas.kinvey.com/rpc/kid_H1o3YfRo/custom/email`,
-      type: 'POST',
-      data: {
-        myEmail: 'hello@shannonriester.com',
-        senderEmail: this.refs.email.value,
-        message: this.refs.message.value
-      },
-      success: (r) => {
-        console.log(r);
-      },
-      error: (r) => {
-        console.log('error', r);
-      }
-    });
-    this.setState({sentEmail: true});
-    window.setTimeout(() => {
-      this.setState({sentEmail: false});
-      this.refs.email.value = '';
-      this.refs.subject.value = '';
-      this.refs.message.value = '';
-    }, 7000);
-
+    if (this.validateEmail(this.refs.email.value) && this.refs.message.value.length > 0 && this.refs.message.value !== ' ' && this.refs.message.value !== '') {
+      $.ajax({
+        url: `https://baas.kinvey.com/rpc/kid_H1o3YfRo/custom/email`,
+        type: 'POST',
+        data: {
+          myEmail: 'hello@shannonriester.com',
+          senderEmail: this.refs.email.value,
+          message: this.refs.message.value
+        },
+        success: (r) => {
+          console.log(r);
+        },
+        error: (r) => {
+          console.log('error', r);
+        }
+      });
+      this.setState({sentEmail: true});
+      window.setTimeout(() => {
+        this.setState({sentEmail: false});
+        this.refs.email.value = '';
+        this.refs.subject.value = '';
+        this.refs.message.value = '';
+      }, 7000);
+    } else {
+      this.setState({shake: true});
+      window.setTimeout(() => {
+        this.setState({shake: false});
+      }, 1000)
+    }
   },
   render() {
     let contact;
@@ -48,15 +62,21 @@ export default React.createClass({
 
       );
     } else {
+      let messageContainer = "message-container";
+      if (this.state.shake) {
+        messageContainer += " shake";
+      }
       contact = (
         <form className="contact-form" onSubmit={this.contactMe}>
           <h2 className="contact-h2">Questions? Thoughts? Let me know.</h2>
           <h4 className="my-email">hello@shannonriester.com</h4>
-          <label className="contact-label">Email</label>
-          <input type="email" className="contact-input" placeholder="Email" tabIndex="1" ref="email"/>
-          <label className="contact-label">Subject</label>
-          <input type="text" className="contact-input" placeholder="Subject" tabIndex="3" ref="subject"/>
-          <textarea className="contact-message" tabIndex="4" ref="message"></textarea>
+          <section className={messageContainer}>
+            <label className="contact-label">Email</label>
+            <input type="email" className="contact-input" placeholder="Email" tabIndex="1" ref="email"/>
+            <label className="contact-label">Subject</label>
+            <input type="text" className="contact-input" placeholder="Subject" tabIndex="3" ref="subject"/>
+            <textarea className="contact-message" tabIndex="4" ref="message"></textarea>
+          </section>
           <input className="contact-btn" type="text" type="submit" role="button" value="Send"/>
         </form>);
     }
